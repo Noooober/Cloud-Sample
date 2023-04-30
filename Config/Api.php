0f6e27098738239fc7922dac723dc5c382272bd6
Noooober <87687703+Noooober@users.noreply.github.com>
2023-05-01 03:46:18 +0800

    Update Api.php

--- Config/Api.php
+++ Config/Api.php
@@ -1,7 +1,7 @@
 <?php
 
 //connection
-define('DB_HOST', 'mbp-db.crr4ck204eku.us-east-1.rds.amazonaws.com');
+define('DB_HOST', 'mbp-db.cuqkfmv8zenx.us-east-1.rds.amazonaws.com');
 define('DB_USER', 'admin');
 define('DB_PASS', 'mondayblues');
 define('DB_NAME', 'MBP');
